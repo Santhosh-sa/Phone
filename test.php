b545129ebbf50ef422c12230e2a6d792f5cda740
grovermind <carltonjacobson@gmail.com>
2013-08-11 22:51:31 -0400

    more cleanup

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$text = "test99 hey";
-$choppedText = substr($text,6);
-//echo $choppedText;
-if (stripos($text, 'test99') == 0) {
-	echo $choppedText;
-}
--- testfunction.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include './functions.php';
-
-SendText('8145215791','There is a pending message for you in the phone broadcast system.');
-?>
